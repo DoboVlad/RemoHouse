15f7bcabdc292a0ffdccc93a7c778e5144a0c270
Nenisca Maria <neniscamaria13@gmail.com>
2020-06-09 12:28:13 +0300

    started validation

--- remohouse-core/src/main/java/org/circuitdoctor/core/model/Location.java
+++ remohouse-core/src/main/java/org/circuitdoctor/core/model/Location.java
@@ -4,6 +4,9 @@ import lombok.*;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 @Entity
@@ -17,9 +20,19 @@ import java.io.Serializable;
 @Table(name="location")
 public class Location extends BaseEntity<Long> implements Serializable {
     @Column(nullable = false, unique = true)
-    private String coordinates;
+    @NotBlank(message = "Latitude is mandatory")
+    @Pattern(regexp = "^(\\+|-)?(?:90(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,6})?))$\n")
+    private String latitude;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Longitude is mandatory")
+    @Pattern(regexp = "^(\\+|-)?(?:180(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,6})?))$")
+    private String longitude;
+
     @Column(nullable = true)
     private String image; //to be updated later
     @Column(nullable = false)
+    @NotBlank(message = "Name is mandatory")
+    @Size()
     private String name;
 }
--- remohouse-core/src/main/java/org/circuitdoctor/core/model/validators/Validator.java
+++
@@ -1,7 +0,0 @@
-package org.circuitdoctor.core.model.validators;
-import javax.persistence.MappedSuperclass;
-
-@MappedSuperclass
-public interface Validator<T> {
-    void validate(T entity) throws ValidatorException;
-}
