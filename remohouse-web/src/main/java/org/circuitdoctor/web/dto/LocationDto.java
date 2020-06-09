dd20ca10026a8d54d3c8a9fd9b6d992b5fce9963
Nenisca Maria <neniscamaria13@gmail.com>
2020-06-09 13:51:54 +0300

    added location dto

--- remohouse-web/src/main/java/org/circuitdoctor/web/dto/LocationDto.java
+++ remohouse-web/src/main/java/org/circuitdoctor/web/dto/LocationDto.java
@@ -1,4 +1,17 @@
 package org.circuitdoctor.web.dto;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Builder
 public class LocationDto extends BaseDto {
+    private String latitude;
+    private String longitude;
+    private String image; //to be updated later
+    private String name;
+    UserDto userDto;
 }
