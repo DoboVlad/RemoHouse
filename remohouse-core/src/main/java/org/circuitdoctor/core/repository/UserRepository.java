8ba6e48b48da8998c6827e788d3af675f6ff3e6b
BangauAndrei <andrei.bangau99@gmail.com>
2020-06-09 12:27:14 +0300

    added user Entity,Repo , Service

---
+++ remohouse-core/src/main/java/org/circuitdoctor/core/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package org.circuitdoctor.core.repository;
+
+import org.circuitdoctor.core.model.User;
+
+public interface UserRepository extends Repository<User,Long> {
+}
---
+++ remohouse-core/src/main/java/org/circuitdoctor/core/service/UserService.java
@@ -0,0 +1,9 @@
+package org.circuitdoctor.core.service;
+
+import org.circuitdoctor.core.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getAllUsers();
+}
