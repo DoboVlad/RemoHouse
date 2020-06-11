63626c08b837a1708a4bff02c352727ee59e287e
BangauAndrei <andrei.bangau99@gmail.com>
2020-06-11 14:11:15 +0300

    dbunit tests on user repo and service

---
+++ remohouse-core/src/test/java/org/circuitdoctor/core/service/ITConfig.java
@@ -0,0 +1,21 @@
+package org.circuitdoctor.core.service;
+
+import org.circuitdoctor.core.config.JPAConfig;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+/**
+ * Created by radu.
+ */
+@Configuration
+@ComponentScan(value = "org.circuitdoctor.core",
+        excludeFilters = {@ComponentScan.Filter(value = {JPAConfig.class}, type = FilterType.ASSIGNABLE_TYPE)})
+@Import({JPAConfigIT.class})
+@PropertySources({@PropertySource(value = "classpath:db-h2.properties")})
+public class ITConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
