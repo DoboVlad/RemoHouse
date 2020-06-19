fd107f7e812cfe5a85fb55a336672d1d780cc8cc
Nenisca Maria <neniscamaria13@gmail.com>
2020-06-19 13:11:15 +0300

    added routing from home

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {SignupLoginComponent} from "./components/signup-login/signup-login.component";
+import {AboutusComponent} from "./components/aboutus/aboutus.component";
+import {GetstartedComponent} from "./components/getstarted/getstarted.component";
+import {HomeComponent} from "./components/home/home.component";
 
 
 const routes: Routes = [
   // { path: '', redirectTo: '/home', pathMatch: 'full' },
-
-
+  {path: 'register', component: SignupLoginComponent},
+  {path: 'about', component: AboutusComponent},
+  {path: 'getstarted', component: GetstartedComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
