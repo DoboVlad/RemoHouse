84154d835b3bb95878528b88b415500138ecc531
Nenisca Maria <neniscamaria13@gmail.com>
2020-06-19 18:30:00 +0300

    finished patching;

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AboutusComponent} from "./components/aboutus/aboutus.component";
 import {HomeComponent} from "./components/home/home.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {AboutusComponent} from "./components/aboutus/aboutus.component";
 import {GetstartedComponent} from "./components/getstarted/getstarted.component";
 
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'about', component: AboutusComponent},
   {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'about', component: AboutusComponent},
   {path: 'getstarted', component: GetstartedComponent}
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,17 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutusComponent } from './components/aboutus/aboutus.component';
-import { HomeComponent } from './components/home/home.component';
-import { RegisterComponent } from './components/register/register.component';
-import { GetstartedComponent } from './components/getstarted/getstarted.component';
+import {HomeComponent} from "./components/home/home.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {AboutusComponent} from "./components/aboutus/aboutus.component";
+import {GetstartedComponent} from "./components/getstarted/getstarted.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutusComponent,
     HomeComponent,
     RegisterComponent,
+    AboutusComponent,
     GetstartedComponent
   ],
   imports: [
