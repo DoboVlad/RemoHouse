8cbe682b8fe43f45c04bc0d28429aabd5697ad6a
Claudia <benko.claudia@yahoo.com>
2020-06-21 14:27:47 +0300

    Made navbar responsive

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'REMO';
+
+  ngOnInit(): void {
+    function show() {
+      const x = document.getElementById("nav");
+      if (x.className === "navbar") {
+        x.className += " responsive";
+      } else {
+        x.className = "navbar";
+      }
+    }
+
+    const icon = document.getElementById("icon");
+    icon.addEventListener("click", show);
+
+    window.onscroll = function() {fixNav()};
+
+    var header = document.getElementById("nav");
+
+    var sticky = header.offsetTop;
+
+    function fixNav() {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    }
+  }
 }
