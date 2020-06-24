5d7ddb9b049575b3690e48992b41df1dc9b6f6b0
DiosDuck <62239146+DiosDuck@users.noreply.github.com>
2020-06-24 17:29:46 +0300

    Added youraccount page

--- front-end/src/app/components/account/account.component.ts
+++ front-end/src/app/components/account/account.component.ts
@@ -8,13 +8,26 @@ import {Router} from "@angular/router";
 })
 export class AccountComponent implements OnInit {
 
+  name:string;
+  surname:string;
+  email:string;
+  phone:string;
+
   constructor(private router : Router) { }
 
   ngOnInit(): void {
+    this.name="Bob";
+    this.surname="Ross";
+    this.email="bob.ross@happy_accidents.com";
+    this.phone="07ceva0123";
   }
 
   logOut() {
     localStorage.setItem("user",null);
     this.router.navigate(["/home"]);
+  }
+
+  changePassword() {
+
   }
 }
