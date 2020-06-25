ebae9b669161aa88b2a3eb69d27080cd18f41625
Nenisca Maria <neniscamaria13@gmail.com>
2020-06-25 10:47:32 +0300

    f

--- front-end/src/app/model/GSMController.ts
+++ front-end/src/app/model/GSMController.ts
@@ -3,12 +3,14 @@ export class GSMController {
   roomID : number;
   phoneNumber : string;
   status : string;
+  type : string;
 
-  constructor(id: number, roomID: number, phoneNumber: string, status: string) {
+
+  constructor(id: number, roomID: number, phoneNumber: string, status: string, type: string) {
     this.id = id;
     this.roomID = roomID;
     this.phoneNumber = phoneNumber;
     this.status = status;
+    this.type = type;
   }
-
 }
