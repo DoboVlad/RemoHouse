7b47671f0b4f23e8f159189053c65496290cbc3c
DoboVlad <66554619+DoboVlad@users.noreply.github.com>
2020-07-04 12:08:29 +0300

    Made forgot password page work

--- front-end/src/app/components/forgot-password/forgot-password.component.ts
+++ front-end/src/app/components/forgot-password/forgot-password.component.ts
@@ -10,6 +10,25 @@ export class ForgotPasswordComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  resetPassword(){
+    document.getElementById("titlu").innerHTML="You can reset your password here!";
+    document.getElementById("reset").style.display="none";
+
+    document.getElementById("resetPhone").style.display="inline-block";
+  }
+  resetPassword1(){
+    document.getElementById("titlu").innerHTML="You can reset your password here!";
+    document.getElementById("reset").style.display="none";
+
+    document.getElementById("resetEmail").style.display="inline-block";
+  }
+  cancel(){
+    document.getElementById("titlu").innerHTML="How would you like to reset your password?";
+    document.getElementById("reset").style.display="inline";
+
+    document.getElementById("resetEmail").style.display="none";
+    document.getElementById("resetPhone").style.display="none";
+  }
 }
