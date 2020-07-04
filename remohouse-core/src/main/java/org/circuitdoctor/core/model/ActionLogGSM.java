6a06ccc385682319f5629fbdd6cd035f94bb3e93
Nenisca Maria <neniscamaria13@gmail.com>
2020-07-04 14:59:15 +0300

    removed validations from action log

--- remohouse-core/src/main/java/org/circuitdoctor/core/model/ActionLogGSM.java
+++ remohouse-core/src/main/java/org/circuitdoctor/core/model/ActionLogGSM.java
@@ -20,13 +20,8 @@ import java.time.LocalDateTime;
 @Table(name="action_log_gsm")
 public class ActionLogGSM extends BaseEntity<Long> implements Serializable {
     @Column(nullable = false)
-    @NonNull
-    @NotBlank(message = "operationType is mandatory")
     private String operationType;
-
     @Column(nullable = false,unique = true)
-    @NonNull
-    @NotBlank(message = "date is mandatory")
     private LocalDateTime dateTime;
     @ManyToOne(fetch = FetchType.EAGER)
     private User user;
