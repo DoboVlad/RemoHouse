0eec7352d050444305cbced7d2e0d8bcae3dc517
DoboVlad <66554619+DoboVlad@users.noreply.github.com>
2020-07-05 15:42:19 +0300

    Added a final to form

--- front-end/src/app/components/forgot-password/forgot-password.component.ts
+++ front-end/src/app/components/forgot-password/forgot-password.component.ts
@@ -7,28 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
-
   }
-  resetPassword(){
-    document.getElementById("titlu").innerHTML="You can reset your password here!";
-    document.getElementById("reset").style.display="none";
 
-    document.getElementById("resetPhone").style.display="inline-block";
+  resetPassword() {
+    document.getElementById("titlu").innerHTML = "You can reset your password here!<br> Just enter your phone number!";
+    document.getElementById("reset").style.display = "none";
+    document.getElementById("resetPhone").style.display = "inline-block";
   }
-  resetPassword1(){
-    document.getElementById("titlu").innerHTML="You can reset your password here!";
-    document.getElementById("reset").style.display="none";
 
-    document.getElementById("resetEmail").style.display="inline-block";
+  resetPassword1() {
+    document.getElementById("titlu").innerHTML = "You can reset your password here!<br> Just enter your email!";
+    document.getElementById("reset").style.display = "none";
+    document.getElementById("resetEmail").style.display = "inline-block";
   }
-  cancel(){
-    document.getElementById("titlu").innerHTML="How would you like to reset your password?";
-    document.getElementById("reset").style.display="inline";
 
-    document.getElementById("resetEmail").style.display="none";
-    document.getElementById("resetPhone").style.display="none";
+  cancel() {
+    document.getElementById("titlu").innerHTML = "How would you like to reset your password?";
+    document.getElementById("reset").style.display = "inline";
+    document.getElementById("resetEmail").style.display = "none";
+    document.getElementById("resetPhone").style.display = "none";
+  }
+
+  reset(c) {
+    document.getElementById("titlu").innerHTML = "Please enter your code below!";
+    document.getElementById("reset1").style.display = "block";
+    document.getElementById("reset").style.display = "none";
+    document.getElementById("resetEmail").style.display = "none";
+    document.getElementById("resetPhone").style.display = "none";
+    document.getElementById("span1").innerHTML=c;
+  }
+  continue(){
+    document.getElementById("titlu").innerHTML = "Choose a new password!";
+    document.getElementById("reset1").style.display = "none";
+    document.getElementById("reset").style.display = "none";
+    document.getElementById("resetEmail").style.display = "none";
+    document.getElementById("resetPhone").style.display = "none";
+    document.getElementById("continue").style.display="inline";
   }
 }
