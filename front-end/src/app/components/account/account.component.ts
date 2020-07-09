0fa8651334a8b2c12ea0df04b18ac6a7fe4d6abd
Nenisca Maria <neniscamaria13@gmail.com>
2020-07-09 16:45:06 +0300

    f

--- front-end/src/app/components/account/account.component.ts
+++ front-end/src/app/components/account/account.component.ts
@@ -55,47 +55,134 @@ newPassword: string;}
     ]),
   ],
 })
-export class AccountComponent implements OnInit {
+  export class AccountComponent implements OnInit {
 
-  user: User;
-  page: string;
-  locations: LocationModel[];
+    user: User;
+    page: string;
+    locations: LocationModel[];
 
-  expandedLocation: LocationModel | null;
-  locationColumns: string[] = ["name", "latitude", "longitude", "city"];
-  locationDataSource;
+    expandedLocation: LocationModel | null;
+    locationColumns: string[] = ["name", "latitude", "longitude", "city"];
+    locationDataSource;
 
-  expandedRoom: Room | null;
-  roomColumns: string[] = ["name"];
-  roomDataSource;
-  rooms: Room[];
+    expandedRoom: Room | null;
+    roomColumns: string[] = ["name"];
+    roomDataSource;
+    rooms: Room[];
 
-  expandedController: GSMController | null;
-  controllerColumns: string[] = ["type", "status", "phoneNumber"];
-  controllerDataSource;
-  gsmController: GSMController[];
-  room: Room;
+    expandedController: GSMController | null;
+    controllerColumns: string[] = ["type", "status", "phoneNumber"];
+    controllerDataSource;
+    gsmController: GSMController[];
+    room: Room;
 
-  applyFilter(event: Event, dataSource) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    dataSource.filter = filterValue.trim().toLowerCase();
-  }
-
-  constructor(
-    public dialog: MatDialog,
-    private router: Router,
-    private userService: UserService,
-    private locationService: LocationService,
-    private roomService: RoomService,
-    private gsmControllerService: GsmControllerService,
-    public snackBar: MatSnackBar) {
-    if (localStorage.getItem("user") == "null") {
-      this.router.navigate(["/unauthorizedaccess"]);
+    applyFilter(event: Event, dataSource) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      dataSource.filter = filterValue.trim().toLowerCase();
     }
-  }
 
-  AddGSM() {
-        const dialogRef=this.dialog.open(AddGSMComponent,{
+    constructor(
+      public dialog: MatDialog,
+      private router: Router,
+      private userService: UserService,
+      private locationService: LocationService,
+      private roomService: RoomService,
+      private gsmControllerService: GsmControllerService,
+      public snackBar: MatSnackBar) {
+      if (localStorage.getItem("user") == "null") {
+        this.router.navigate(["/unauthorizedaccess"]);
+      }
+    }
+
+    ngOnInit(): void {
+      this.page = "Profile";
+      var aux = localStorage.getItem("user");
+      this.userService.getUserByCredential(aux).subscribe(user => {
+        this.user = user;
+        this.locationService.getLocations(this.user.id).subscribe(locations => {
+          this.locations = locations;
+          this.locationDataSource = new MatTableDataSource<LocationModel>(locations);
+        });
+      });
+    }
+    logOut() {
+      localStorage.setItem("user", null);
+      this.router.navigate(["/home"]);
+    }
+    checkPassword(password: string): boolean {
+      //just to play around
+      var rez = true;
+      if (password.length < 7) {
+        rez = false;
+      }
+      return rez;
+
+    }
+
+    //functions to determine the page
+    isProfile() {
+      return this.page === "Profile";
+    }
+    isSecurity() {
+      return this.page === "Security";
+    }
+    isManageLocations() {
+      return this.page === "ManageLocations";
+    }
+    isPastActions() {
+    return this.page === "PastActions"
+  }
+    setProfile(){
+      this.page="Profile";
+    }
+    setManageLocations() {
+      this.page = "ManageLocations"
+    }
+    setSecurity() {
+      this.page = "Security"
+    }
+    setPastAction() {
+      this.page = "PastActions"
+    }
+
+    changePassword2(oldPassword: string, newPassword: string, confirmPassword: string) {
+      if (this.user.password === oldPassword) {
+        if (newPassword === confirmPassword) {
+          if (!this.checkPassword(newPassword)) {
+            this.snackBar.open("The new password is too short.", "Ok", {duration: 2000});
+          } else {
+            this.user.password = newPassword;
+            console.log("changing password ", newPassword);
+            this.userService.changePassword(this.user.id, this.user).subscribe(response => {
+              console.log(response);
+              this.snackBar.open("Password changed.", "Ok", {duration: 2000});
+            });
+          }
+        } else {
+          this.snackBar.open("The new password and confirm password are different.", "Ok", {duration: 2000});
+        }
+      } else {
+        this.snackBar.open("The old password is incorect.", "Ok", {duration: 2000});
+      }
+    }
+
+    toggleLocationRow(location: LocationModel) {
+      this.expandedLocation = this.expandedLocation === location ? null : location;
+      this.roomService.getRooms(this.user.id, location.id).subscribe(rooms => {
+        this.roomDataSource = new MatTableDataSource<Room>(rooms);
+      });
+    }
+
+    toggleRooms(room: Room) {
+      this.expandedRoom = this.expandedRoom === room ? null : room;
+      this.gsmControllerService.getGSMs(this.user.id, room.id).subscribe(gsms => {
+        this.controllerDataSource = new MatTableDataSource<GSMController>(gsms);
+      })
+    }
+
+    //GSM curd function
+    AddGSM() {
+      const dialogRef=this.dialog.open(AddGSMComponent,{
         width: '300px',
         data:{type:"Add"}
       });
@@ -116,203 +203,98 @@ export class AccountComponent implements OnInit {
         }
       });
     }
-
-  UpdateGSM(controller: any){
-    const dialogRef=this.dialog.open(UpdateGSMComponent,{
-      width: '300px',
-      data:{type:"Update",
-        gsm_type: controller["gsm_type"],
-        status: controller["status"],
-        phoneNumber: controller["phoneNumber"]
-      }
-    });
-    dialogRef.afterClosed().subscribe(result=>{
-      console.log('The dialog was closed');
-      console.log(result);
-      if(result!=null){
-        var gsmcontroller = new GSMController(-1,-1,result.phoneNumber,result.status,result.gsm_type);
-        this.snackBar.open(String("Updated GSM"),"ok",{duration:2000});
-        /*this.gsmControllerService.openGSM(gsmcontroller, this.user.id).subscribe(controller =>{
-          this.gsmController=controller;
-          this.controllerDataSource= new MatTableDataSource<GSMController>(controller);
-        })*/
-      }
-    })
-  }
-
-  DeleteGSM(controller:any){
-    const dialogRef=this.dialog.open(DeleteGSMComponent);
-   /* const dialogRef=this.dialog.open(DeleteGSMComponent,{
-      width: '300px',
-      data:{type:"Delete",
-        gsm_type:controller["gsm_type"],
-        status: controller["status"],
-        phoneNumber: controller["phoneNumber"]}
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      console.log(result);
-      if(result!=null) {
-        this.snackBar.open(String("Deleted GSM."),"Ok",{duration:2000});
-      }
-    });*/
-  }
-
-  setProfile(){
-    this.page="Profile";
-  }
-
-  setManageLocations() {
-    this.page = "ManageLocations"
-  }
-
-  setManageRooms() {
-    this.page = "ManageRooms"
-  }
-
-  setManageController() {
-    this.page = "ManageController"
-  }
-
-  setSecurity() {
-    this.page = "Security"
-  }
-
-  ngOnInit(): void {
-    this.page = "Profile";
-    var aux = localStorage.getItem("user");
-    this.userService.getUserByCredential(aux).subscribe(user => {
-      this.user = user;
-      this.locationService.getLocations(this.user.id).subscribe(locations => {
-        this.locations = locations;
-        this.locationDataSource = new MatTableDataSource<LocationModel>(locations);
-      });
-    });
-  }
-
-  logOut() {
-    localStorage.setItem("user", null);
-    this.router.navigate(["/home"]);
-  }
-
-  checkPassword(password: string): boolean {
-    //just to play around
-    var rez = true;
-    if (password.length < 7) {
-      rez = false;
-    }
-    return rez;
-
-  }
-
-  isProfile() {
-    return this.page === "Profile";
-  }
-
-  isSecurity() {
-    return this.page === "Security";
-  }
-
-  isManageLocations() {
-    return this.page === "ManageLocations";
-  }
-
-  changePassword2(oldPassword: string, newPassword: string, confirmPassword: string) {
-    if (this.user.password === oldPassword) {
-      if (newPassword === confirmPassword) {
-        if (!this.checkPassword(newPassword)) {
-          this.snackBar.open("The new password is too short.", "Ok", {duration: 2000});
-        } else {
-          this.user.password = newPassword;
-          console.log("changing password ", newPassword);
-          this.userService.changePassword(this.user.id, this.user).subscribe(response => {
-            console.log(response);
-            this.snackBar.open("Password changed.", "Ok", {duration: 2000});
-          });
+    UpdateGSM(controller: any){
+      const dialogRef=this.dialog.open(UpdateGSMComponent,{
+        width: '300px',
+        data:{type:"Update",
+          gsm_type: controller["gsm_type"],
+          status: controller["status"],
+          phoneNumber: controller["phoneNumber"]
         }
-      } else {
-        this.snackBar.open("The new password and confirm password are different.", "Ok", {duration: 2000});
-      }
-    } else {
-      this.snackBar.open("The old password is incorect.", "Ok", {duration: 2000});
-    }
-  }
-
-  toggleLocationRow(location: LocationModel) {
-    this.expandedLocation = this.expandedLocation === location ? null : location;
-    this.roomService.getRooms(this.user.id, location.id).subscribe(rooms => {
-      this.roomDataSource = new MatTableDataSource<Room>(rooms);
-    });
-  }
-
-  toggleRooms(room: Room) {
-    this.expandedRoom = this.expandedRoom === room ? null : room;
-    this.gsmControllerService.getGSMs(this.user.id, room.id).subscribe(gsms => {
-      this.controllerDataSource = new MatTableDataSource<GSMController>(gsms);
-    })
-  }
-
-  addRoom() {
-    const dialogRef = this.dialog.open(AddRoomComponent)
-    dialogRef.afterClosed().subscribe(name => {
-      console.log(name);
-        if (name != null) { //fix this
-          var room = new Room(-1, this.expandedLocation.id, name);
-          this.roomService.addRoom(this.user.id, room).subscribe(response => {
-            this.snackBar.open(String(" The room has been added"), "OK", {duration: 2000})
-            this.roomService.getRooms(this.user.id, this.expandedLocation.id).subscribe(rooms => {
-              this.rooms=rooms;
-              this.roomDataSource=new MatTableDataSource<Room>(rooms);
-            })
-          });
+      });
+      dialogRef.afterClosed().subscribe(result=>{
+        console.log('The dialog was closed');
+        console.log(result);
+        if(result!=null){
+          var gsmcontroller = new GSMController(-1,-1,result.phoneNumber,result.status,result.gsm_type);
+          this.snackBar.open(String("Updated GSM"),"ok",{duration:2000});
+          /*this.gsmControllerService.openGSM(gsmcontroller, this.user.id).subscribe(controller =>{
+            this.gsmController=controller;
+            this.controllerDataSource= new MatTableDataSource<GSMController>(controller);
+          })*/
         }
       })
-  }
-
-  deleteRoom() {
-    const dialogRef = this.dialog.open(DeleteRoomComponent)
-    dialogRef.afterClosed().subscribe(result => {
-        if (result == true) { //fix this
-        /*  var room = new Room(this.expandedRoom.id,this.expandedLocation.id, this.expandedRoom.name);
-          this.roomService.deleteRoom(this.user.id, this.expandedRoom.id).subscribe(response =>{
-          this.snackBar.open(String(" The room has been deleted"), "OK", {duration: 2000})
-          this.roomService.getRooms(this.user.id, this.expandedLocation.id).subscribe(rooms => {
-            this.rooms = rooms;
-            this.roomDataSource = new MatTableDataSource<Room>(rooms);
-          })
-        }) */
-        }
-      }
-    )
-  }
-
-  updateRoom() {
-    const dialogRef = this.dialog.open(UpdateRoomComponent)
-    dialogRef.afterClosed().subscribe(name => {
-        if (name != null) { //fix this
-          var room = new Room(this.expandedRoom.id,this.expandedLocation.id, name);
-          this.roomService.updateRoom(this.user.id, room).subscribe(response => {
-            this.snackBar.open(String(" The room has been updated"), "OK",{duration:2000})
-            this.roomService.getRooms(this.user.id, this.expandedLocation.id).subscribe(rooms => {
-              this.rooms=rooms;
-              this.roomDataSource=new MatTableDataSource<Room>(rooms);
-            })
-          });
-        }
+    }
+    DeleteGSM(controller:any){
+      const dialogRef=this.dialog.open(DeleteGSMComponent);
+      /* const dialogRef=this.dialog.open(DeleteGSMComponent,{
+         width: '300px',
+         data:{type:"Delete",
+           gsm_type:controller["gsm_type"],
+           status: controller["status"],
+           phoneNumber: controller["phoneNumber"]}
+       });
+       dialogRef.afterClosed().subscribe(result => {
+         console.log('The dialog was closed');
+         console.log(result);
+         if(result!=null) {
+           this.snackBar.open(String("Deleted GSM."),"Ok",{duration:2000});
+         }
+       });*/
+    }
+  
+    //CRUD room
+    addRoom() {
+      const dialogRef = this.dialog.open(AddRoomComponent)
+      dialogRef.afterClosed().subscribe(name => {
+        console.log(name);
+          if (name != null) { //fix this
+            var room = new Room(-1, this.expandedLocation.id, name);
+            this.roomService.addRoom(this.user.id, room).subscribe(response => {
+              this.snackBar.open(String(" The room has been added"), "OK", {duration: 2000})
+              this.roomService.getRooms(this.user.id, this.expandedLocation.id).subscribe(rooms => {
+                this.rooms=rooms;
+                this.roomDataSource=new MatTableDataSource<Room>(rooms);
+              })
+            });
+          }
         })
-      }
-
-
-
-    isPastActions()
-    {
-      return this.page === "PastActions"
     }
-
-    setPastAction()
-    {
-      this.page = "PastActions"
+    deleteRoom() {
+      const dialogRef = this.dialog.open(DeleteRoomComponent)
+      dialogRef.afterClosed().subscribe(result => {
+          if (result == true) { //fix this
+          /*  var room = new Room(this.expandedRoom.id,this.expandedLocation.id, this.expandedRoom.name);
+            this.roomService.deleteRoom(this.user.id, this.expandedRoom.id).subscribe(response =>{
+            this.snackBar.open(String(" The room has been deleted"), "OK", {duration: 2000})
+            this.roomService.getRooms(this.user.id, this.expandedLocation.id).subscribe(rooms => {
+              this.rooms = rooms;
+              this.roomDataSource = new MatTableDataSource<Room>(rooms);
+            })
+          }) */
+          }
+        }
+      )
     }
+    updateRoom() {
+      const dialogRef = this.dialog.open(UpdateRoomComponent)
+      dialogRef.afterClosed().subscribe(name => {
+          if (name != null) { //fix this
+            var room = new Room(this.expandedRoom.id,this.expandedLocation.id, name);
+            this.roomService.updateRoom(this.user.id, room).subscribe(response => {
+              this.snackBar.open(String(" The room has been updated"), "OK",{duration:2000})
+              this.roomService.getRooms(this.user.id, this.expandedLocation.id).subscribe(rooms => {
+                this.rooms=rooms;
+                this.roomDataSource=new MatTableDataSource<Room>(rooms);
+              })
+            });
+          }
+          })
+        }
+
+
+
+
 
 
     addLocation(){
