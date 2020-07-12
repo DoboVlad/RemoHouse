bbb5710d37a77bbb78b213e6aee4d4d23a27a9c3
Claudia <benko.claudia@yahoo.com>
2020-07-12 11:54:52 +0300

    Fixed main-page controllers

--- front-end/src/app/components/controller-toggle/controller-toggle.component.ts
+++ front-end/src/app/components/controller-toggle/controller-toggle.component.ts
@@ -1,30 +1,40 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {MatSlideToggleChange} from "@angular/material/slide-toggle";
+import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {MatSlideToggle, MatSlideToggleChange} from "@angular/material/slide-toggle";
 import {GSMController} from "../../model/GSMController";
 import {DeleteButtonDialogComponent} from "../delete-button-dialog/delete-button-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {Observable} from "rxjs";
+import {GsmControllerService} from "../../service/gsmControllerService";
+import {User} from "../../model/user";
+import {UserService} from "../../service/userService";
 
 @Component({
   selector: 'app-controller-toggle',
   templateUrl: './controller-toggle.component.html',
   styleUrls: ['./controller-toggle.component.css']
 })
-export class ControllerToggleComponent implements OnInit {
+export class ControllerToggleComponent implements OnInit, AfterViewInit {
 
   @Input() controller: GSMController;
   @Output() notify: EventEmitter<GSMController> = new EventEmitter<GSMController>();
+  @ViewChild('controllerToggle') toggle : MatSlideToggle;
+  user: User;
 
-  constructor(private dialog: MatDialog, public snackBar: MatSnackBar) { }
+  constructor(private dialog: MatDialog, public snackBar: MatSnackBar, private gsmService: GsmControllerService, private userService: UserService) { }
 
   ngOnInit(): void {
-    console.log(this.controller.id);
+    this.userService.getUserByCredential(localStorage.getItem("user")).subscribe(user => {
+      this.user = user;
+    });
   }
 
+
   deleteButton() {
-    const dialogRef = this.dialog.open(DeleteButtonDialogComponent)
+    const dialogRef = this.dialog.open(DeleteButtonDialogComponent);
     dialogRef.afterClosed().subscribe(result => {
         if (result == true) { //fix this
+          this.gsmService.deleteGSM(this.user.id, this.controller.id);
           this.openSnackBar("The controller was deleted", "OK");
         }
       }
@@ -41,8 +51,41 @@ export class ControllerToggleComponent implements OnInit {
   }
 
   doorChange($event: MatSlideToggleChange) {
-    console.log("toggle bitch");
-    this.controller.status = this.controller.status == 'ON' ? 'OFF' : 'ON';
+    if(this.controller.status == "OFF") {
+      this.gsmService.openGSM(this.controller, this.user.id).subscribe(response => {
+        if (response) {
+          this.openSnackBar(`Opened ${this.controller.type}`, "OK");
+        } else {
+          this.openSnackBar("Something went wrong", "OK");
+          this.toggle.toggle();
+        }
+      });
+    }
+    else if(this.controller.status == "ON"){
+      this.gsmService.closeGSM(this.controller, this.user.id).subscribe(response => {
+        if (response) {
+          this.openSnackBar(`Closed ${this.controller.type}`, "OK");
+
+        } else {
+          this.openSnackBar("Something went wrong", "OK");
+          this.toggle.toggle();
+        }
+      });
+    }
+    if(this.controller.status == "ON"){
+      this.controller.status = "OFF";
+    }
+    else{
+      this.controller.status = "ON";
+    }
+    this.notify.emit(this.controller)
+  }
+
+  ngAfterViewInit(): void {
+    console.log(`entered after init ${this.controller.id} ${this.controller.type} ${this.controller.status}`);
     this.notify.emit(this.controller);
+    if(this.controller.status==="ON"){
+      this.toggle.toggle();
+    }
   }
 }
